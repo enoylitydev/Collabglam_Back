2c66a4c0a3e9c6644f7119e36d35c6ea29a3c7de
Devansh <devanshd78@gmail.com>
2026-01-05 14:16:46 +0530

    updates

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -10,7 +10,7 @@ const MilestonePayment = require("../models/milestonePayment");
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-const clientUrl = (process.env.CLIENT_URL || "http://localhost:3000").replace(/\/$/, "");
+const clientUrl = (process.env.CAMPAIGN_BASE_URL || "https://collabglam.com").replace(/\/$/, "");
 const brandSuccessPath = process.env.STRIPE_BRAND_SUCCESS_PATH || "/brand/subscriptions";
 const influencerSuccessPath = process.env.STRIPE_INFLUENCER_SUCCESS_PATH || "/influencer/subscriptions";
 const milestoneSuccessPath =
